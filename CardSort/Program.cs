b7b9f8d1a2c400436ea1e8e9f30098946115191e
lokimora <lokki.lam@gmail.com>
2017-02-02 18:55:28 +0300

    Init

---
+++ CardSort/Program.cs
@@ -0,0 +1,27 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            var tickets = TicketHelper.GenerateTickets();
+
+            TicketHelper.DisplayTickets(tickets);
+
+            Console.WriteLine("****-------------------------*****");
+
+            var sorted = TicketSortManager.Sort(tickets);
+
+            TicketHelper.DisplayTickets(sorted);
+        }
+    }
+}
---
+++ Domain/Ticket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Ticket
+    {
+        public Ticket(string From, string To)
+        {
+            this.From = From;
+            this.To = To;
+        }
+
+        public string From { get; set; }
+
+        public string To { get; set; }
+    }
+}
